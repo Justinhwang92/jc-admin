39debb0f0ac77db153822a61b2d15b74517ce803
justinhwang92 <justinhwang92@gmail.com>
2023-04-05 16:05:28 -0400

    Add router setup for the expected screens

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
 import { ColorModeContext, useMode } from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
-import { Topbar } from './screens/global/Topbar';
+import { Topbar, Sidebar } from './screens/global';
+import {
+  Dashboard,
+  Team,
+  Invoices,
+  Contacts,
+  Bar,
+  Form,
+  Line,
+  Pie,
+  FAQ,
+  Geography,
+  Calendar,
+} from './screens';
 
 function App() {
   const { theme, colorMode } = useMode();
@@ -12,6 +26,19 @@ function App() {
         <div className="App">
           <main className="content">
             <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/geography" element={<Geography />} />
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
